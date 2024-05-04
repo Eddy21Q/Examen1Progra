328e147bc782db0a37e724cf8f3111cefe9de7c2
Eddy21Q <josue.72.gonza2023@gmail.com>
2024-05-03 22:10:47 -0600

    tttttttt

--- ProtecciónAnimales/src/App.java
+++ ProtecciónAnimales/src/App.java
@@ -3,28 +3,10 @@ public class App {
 
 
 
-        System.out.println("Hello, World!");
+        System.out.println("Aqui se hará el desarrollo de la lógica del proyecto");
 
-       System.out.println("700000");
 
-        System.out.println();
-        System.out.println("xd");
 
-        System.out.println("300");
 
-        
-        System.out.println("11111");
-
-         System.out.println("JJJJJJJJJJJJJJJ");
-        System.out.println("000000000000");
-
-        System.out.println("111111111111111111111111111111111");
-
-        System.out.println("xxxxxxxxxxxxxx");
-
-       
-        System.out.println("kkkkkkkkkkkkkk");
-        System.out.println("1222");
-       System.out.println("PRUEBAAAA");
     }
 }
