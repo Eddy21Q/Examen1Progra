74eec32eb2bb155e45523fcbefb5d66b2ea7a44c
Eddy21Q <josue.72.gonza2023@gmail.com>
2024-05-03 21:21:22 -0600

    cambio

--- ProtecciónAnimales/src/App.java
+++ ProtecciónAnimales/src/App.java
@@ -1,11 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+
+
         System.out.println("Hello, World!");
 
+     
+
 
         System.out.println("Hello, World!");
 
+     
+        System.out.println("Hello, World!");
+        System.out.println("300");
 
-        System.out.println();
+
+
+
+        
     }
 }
