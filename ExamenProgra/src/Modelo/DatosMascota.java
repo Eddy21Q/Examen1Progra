32d8209a334b651ea849fd1feca8d003e3757156
Sioxs81 <demiansand@gmail.com>
2024-05-04 17:49:00 -0600

    CAMBIOS SOBRE LA HERENCIA

--- ExamenProgra/src/Modelo/DatosMascota.java
+++ ExamenProgra/src/Modelo/DatosMascota.java
@@ -1,19 +1,19 @@
 package Modelo;
 
-public class DatosMascota {
+public abstract class DatosMascota {
 
 
     private String nombre;
     private String raza;
     private int edad;
-    private boolean esterilizada;
+   
 
     // Constructor
     public DatosMascota(String nombre,String raza, int edad, boolean esterilizada) {
         this.nombre = nombre;
         this.raza = raza;
         this.edad = edad;
-        this.esterilizada = esterilizada;
+       
 
       
     }
