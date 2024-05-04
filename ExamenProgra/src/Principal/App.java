2f0548575083a9f7b592b45de8299a0056616d62
Eddy21Q <josue.72.gonza2023@gmail.com>
2024-05-04 17:12:25 -0600

    Interfaz Grafica

--- ExamenProgra/src/Principal/App.java
+++ ExamenProgra/src/Principal/App.java
@@ -1,12 +1,15 @@
 package Principal;
 
+import Vista.GUI;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
+          GUI pantalla = new GUI();
+          pantalla.setVisible(true);
+          pantalla.setLocationRelativeTo(null);
 
-
-
+   
 
     }
 }
--- ExamenProgra/src/Vista/GUI.java
+++ ExamenProgra/src/Vista/GUI.java
@@ -1,6 +1,56 @@
 package Vista;
 
-public class GUI {
+import java.awt.Color;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI extends JFrame{
+
+public GUI() {
+        setTitle("Aplicación Protectora de Animales");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setLocationRelativeTo(null); // Centrar la ventana
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.GREEN); // Fondo verde
+
+        // Layout vertical
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        // Botones
+        JButton btnDarEnAdopcion = new JButton("Dar mascota en adopción");
+        JButton btnMostrarMascotas = new JButton("Mostrar mascotas disponibles para adoptar");
+        JButton btnEsterilizacion = new JButton("¿Qué es la esterilización y cómo cuidar nuestras mascotas?");
+
+        // Añadir botones al panel
+        panel.add(btnDarEnAdopcion);
+        panel.add(btnMostrarMascotas);
+        panel.add(btnEsterilizacion);
+
+        // Añadir panel al JFrame
+        getContentPane().add(panel);
+
+      
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
