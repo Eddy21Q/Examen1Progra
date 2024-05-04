6bbeaea8a009334717f17d392e6cfdc4acb4c854
Eddy21Q <josue.72.gonza2023@gmail.com>
2024-05-04 16:31:51 -0600

    DatosMascota

--- ExamenProgra/src/Modelo/DatosMascota.java
+++ ExamenProgra/src/Modelo/DatosMascota.java
@@ -3,9 +3,23 @@ package Modelo;
 public class DatosMascota {
 
 
+    private String nombre;
+    
+    private String raza;
+    private int edad;
+    private boolean esterilizada;
 
+    // Constructor
+    public DatosMascota(String nombre,String raza, int edad, boolean esterilizada) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = edad;
+        this.esterilizada = esterilizada;
+
+        System.out.println();
     }
+    
 
 
 
-
+}
