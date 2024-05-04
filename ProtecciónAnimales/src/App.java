c689416a94c58da023751369a3192b082682da69
Sioxs81 <demiansand@gmail.com>
2024-05-03 21:55:33 -0600

    KKKKKK

--- ProtecciónAnimales/src/App.java
+++ ProtecciónAnimales/src/App.java
@@ -15,15 +15,15 @@ public class App {
         
         System.out.println("11111");
 
-      System.out.println("JJJJJJJJJJJJJJJ");
+         System.out.println("JJJJJJJJJJJJJJJ");
         System.out.println("000000000000");
 
         System.out.println("111111111111111111111111111111111");
 
         System.out.println("xxxxxxxxxxxxxx");
 
-
-
+       
+        System.out.println("kkkkkkkkkkkkkk");
         System.out.println("1222");
        System.out.println("PRUEBAAAA");
     }
