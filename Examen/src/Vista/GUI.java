6fccb708b62a23cda55e952f33505bc7b47807b8
Eddy21Q <josue.72.gonza2023@gmail.com>
2024-05-04 21:52:47 -0600

    cambios

--- Examen/src/Vista/GUI.java
+++ Examen/src/Vista/GUI.java
@@ -14,6 +14,7 @@ import javax.swing.JPanel;
 public class GUI extends JFrame{
   
     public GUI() {
+        System.out.println("hhhhh");
 
  setTitle("Aplicación Protectora de Animales");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
