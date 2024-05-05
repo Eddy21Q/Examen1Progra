926905efded1e1d5810cbc3e4e41e7a2c790c6c2
Eddy21Q <josue.72.gonza2023@gmail.com>
2024-05-04 21:05:49 -0600

    gui

--- Examen/src/Vista/GUI.java
+++ Examen/src/Vista/GUI.java
@@ -1,16 +1,17 @@
 package Vista;
 
 import java.awt.Color;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GUI extends JFrame{
-
+  
     public GUI() {
 
-        setTitle("Aplicación Protectora de Animales");
+ setTitle("Aplicación Protectora de Animales");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setLocationRelativeTo(null); // Centrar la ventana
@@ -21,6 +22,9 @@ public class GUI extends JFrame{
         // Layout vertical
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
+        // Espaciador vertical
+        panel.add(Box.createVerticalStrut(20)); // Agrega un espacio vertical de 20 píxeles
+
         // Botones
         JButton btnDarEnAdopcion = new JButton("Dar mascota en adopción");
         JButton btnMostrarMascotas = new JButton("Mostrar mascotas disponibles para adoptar");
@@ -28,12 +32,15 @@ public class GUI extends JFrame{
 
         // Añadir botones al panel
         panel.add(btnDarEnAdopcion);
+        panel.add(Box.createVerticalStrut(20)); // Agrega un espacio vertical de 10 píxeles entre los botones
         panel.add(btnMostrarMascotas);
+        panel.add(Box.createVerticalStrut(20)); // Agrega otro espacio vertical de 10 píxeles entre los botones
         panel.add(btnEsterilizacion);
 
         // Añadir panel al JFrame
         getContentPane().add(panel);
 
+        
         
     }
 
