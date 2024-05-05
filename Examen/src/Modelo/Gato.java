45f4972be09e52d5fd94e31b13e97b57fb60b209
Sioxs81 <demiansand@gmail.com>
2024-05-04 20:42:24 -0600

    CONSTRUCTORES

--- Examen/src/Modelo/Gato.java
+++ Examen/src/Modelo/Gato.java
@@ -2,4 +2,9 @@ package Modelo;
 
 public class Gato extends DatosMascota{
 
+    public Gato(String nombre, String raza, int edad, boolean esterilizada) {
+        super(nombre, raza, edad, esterilizada);
+       
+    }
+
 }
--- Examen/src/Modelo/Perro.java
+++ Examen/src/Modelo/Perro.java
@@ -1,6 +1,11 @@
 package Modelo;
 
 public class Perro extends DatosMascota{
+
+    public Perro(String nombre, String raza, int edad, boolean esterilizada) {
+        super(nombre, raza, edad, esterilizada);
+      
+    }
     
  
     
