1f09cbcea9570c475b64d071ff3a89713992cd21
Eddy21Q <josue.72.gonza2023@gmail.com>
2024-05-04 19:53:36 -0600

    aceptar

--- Examen/src/App.java
+++ Examen/src/App.java
@@ -3,9 +3,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-
-        System.out.println("PRUEBA!");
+        
     }
-
+    
 }
