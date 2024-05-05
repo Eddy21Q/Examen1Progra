3fe2335ccad9972def56e989e675b71c083dfe34
Eddy21Q <josue.72.gonza2023@gmail.com>
2024-05-04 22:51:48 -0600

    cambiosGUI

--- Examen/src/Vista/GUI.java
+++ Examen/src/Vista/GUI.java
@@ -10,6 +10,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class GUI extends JFrame{
   
@@ -26,7 +28,9 @@ public class GUI extends JFrame{
 
 
  try {
-            // Lee la imagen del archivo
+            
+            
+             // Lee la imagen del archivo
             Image imagenOriginal = ImageIO.read(new File("C:\\Users\\josue\\Documents\\Examen1Progra\\Examen\\Imagenes\\image.png"));
             
             // Escala la imagen al tamaño deseado
@@ -35,56 +39,51 @@ public class GUI extends JFrame{
             // Crea un ImageIcon con la imagen escalada
             ImageIcon icono = new ImageIcon(imagenEscalada);
             
-            // Crea un JLabel con la imagen
+            // Crea un JLabel con la imagen y lo centra horizontalmente
             JLabel labelImagen = new JLabel(icono);
+            labelImagen.setHorizontalAlignment(SwingConstants.CENTER);
             
-            // Agrega el JLabel al panel en el centro
+            // Agrega el JLabel al panel
             panel.add(labelImagen, BorderLayout.CENTER);
+
+            labelImagen.setBorder(new EmptyBorder(0, 0, 0, 110)); 
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
-        
-        // Layout vertical
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-        // Espaciador vertical
-        panel.add(Box.createVerticalStrut(20)); // Agrega un espacio vertical de 20 píxeles
-
+        JPanel panelBotones = new JPanel();
+        panelBotones.setLayout(new BoxLayout(panelBotones, BoxLayout.Y_AXIS));
+    
         // Botones
         JButton btnDarEnAdopcion = new JButton("Dar mascota en adopción");
         JButton btnMostrarMascotas = new JButton("Mostrar mascotas disponibles para adoptar");
         JButton btnEsterilizacion = new JButton("¿Qué es la esterilización y cómo cuidar nuestras mascotas?");
-
-
-
+    
         Color colorCeleste = new Color(255, 153, 153); // Celeste claro
         btnDarEnAdopcion.setBackground(colorCeleste);
         btnMostrarMascotas.setBackground(colorCeleste);
         btnEsterilizacion.setBackground(colorCeleste);
-
-
+    
         Font nuevaFuente = new Font("Arial", Font.BOLD, 16);
         btnDarEnAdopcion.setFont(nuevaFuente);
         btnMostrarMascotas.setFont(nuevaFuente);
         btnEsterilizacion.setFont(nuevaFuente);
-
-
-
+    
         btnDarEnAdopcion.setAlignmentX(Component.CENTER_ALIGNMENT);
         btnMostrarMascotas.setAlignmentX(Component.CENTER_ALIGNMENT);
         btnEsterilizacion.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-
-        // Añadir botones al panel
-        panel.add(btnDarEnAdopcion);
-        panel.add(Box.createVerticalStrut(20)); // Agrega un espacio vertical de 10 píxeles entre los botones
-        panel.add(btnMostrarMascotas);
-        panel.add(Box.createVerticalStrut(20)); // Agrega otro espacio vertical de 10 píxeles entre los botones
-        panel.add(btnEsterilizacion);
-
-        
-
+    
+        // Añadir botones al panel de botones
+        panelBotones.add(btnDarEnAdopcion);
+        panelBotones.add(Box.createVerticalStrut(20)); // Espacio entre botones
+        panelBotones.add(btnMostrarMascotas);
+        panelBotones.add(Box.createVerticalStrut(20)); // Espacio entre botones
+        panelBotones.add(btnEsterilizacion);
+    
+        // Agregar el panel de botones al panel principal en el este
+        panel.add(panelBotones, BorderLayout.EAST);
+    
         // Añadir panel al JFrame
         getContentPane().add(panel);
 
