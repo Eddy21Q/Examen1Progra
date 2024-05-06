61c9642be9436eb477a3566bad08497f855c43dd
Sioxs81 <demiansand@gmail.com>
2024-05-05 22:28:21 -0600

    ComentariosFinales

--- Examen/src/Controlador/Control.java
+++ Examen/src/Controlador/Control.java
@@ -4,9 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Control implements ActionListener{
-    private GUI vista;
+    private GUI vista;//creacion de instancia de la clase GUI
 
-    public Control(GUI vista) {
+    public Control(GUI vista) {//constructor donde se instancian las acciones de los botones de GUI
         this.vista = vista;
         this.vista.addDarEnAdopcionListener(this);
         this.vista.addMostrarMascotasListener(this);
@@ -14,7 +14,7 @@ public class Control implements ActionListener{
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {//metodo que controla las acciones de los botones importantes de GUI
         String comando = e.getActionCommand();
 
         if (comando.equals("DarEnAdopcion")) {
--- Examen/src/Modelo/Perro.java
+++ Examen/src/Modelo/Perro.java
@@ -1,14 +1,14 @@
 package Modelo;
 
-public class Perro extends DatosMascota {
+public class Perro extends DatosMascota {//herencia de la clase DatosMascota
 
-    public Perro(String nombre, String raza, int edad, boolean esterilizada) {
-        super(nombre, raza, edad, esterilizada);
+    public Perro(String nombre, String raza, int edad, boolean esterilizada) {//constructor
+        super(nombre, raza, edad, esterilizada);//llamado al constructor de la clase padre
 
     }
 
     @Override
-    public String toString() {
+    public String toString() {//metodo para mostrar los datos de los perros ingresados
         return "Perro\n" +
                 "nombre: " + getNombre() + "\n" +
                 "raza: " + getRaza() + "\n" +
