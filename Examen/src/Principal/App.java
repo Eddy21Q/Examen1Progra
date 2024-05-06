a424a60a0f7bcefd2c052faecc59800393d92065
Eddy21Q <josue.72.gonza2023@gmail.com>
2024-05-05 23:02:28 -0600

    comentariosCodigo

--- Examen/src/Principal/App.java
+++ Examen/src/Principal/App.java
@@ -6,10 +6,10 @@ public class App {
     public static void main(String[] args) throws Exception {// Main
 
         GUI pantalla = new GUI();// instancia de la clase GUI para visualizar la interfaz grafica
-        pantalla.setVisible(true);
-        pantalla.setSize(1000, 800);
-        pantalla.setResizable(false);
-        pantalla.setLocationRelativeTo(null);
+        pantalla.setVisible(true);//para que se muestre la pantalla o interfaz
+        pantalla.setSize(1000, 800);// se le define un tamaño
+        pantalla.setResizable(false);//esto evita que el usuario modifique el tamaño de la ventana
+        pantalla.setLocationRelativeTo(null);//para que aprezca la interfaz centrada en la pantalla
 
     }
 }
